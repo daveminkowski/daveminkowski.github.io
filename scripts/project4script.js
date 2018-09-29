5a71b9ff67c343633efd4a4f6089448ed1c4c81e
Dave M <42705084+daveminkowski@users.noreply.github.com>
2018-09-29 14:47:32 -0700

    Project 4 Update
    Updated with project4.html and both scripts to validate the contact forms in the project4 page and the contact page. Both forms now work (sorta) with a simulated redirect from the page (no php or server side handling yet) using a different (but essentially identical in function) script for each. Minor css styling changes for the website as a whole. Fixed the margin-bottom on the sidebar element which was appearing much longer than it needed to.

---
+++ scripts/project4script.js
@@ -0,0 +1,105 @@
+	// project 4's script is slightly different than the script used on contact.html due to formatting and such. The website.contactvalidator.js
+	// file does not include commenting.
+
+	// variables for functions
+	var nameBox = document.getElementById("name");							// name text field
+	var emailBox = document.getElementById("email");						// email text field
+	var messageBox = document.getElementById("message");					// message text field
+	var notice = document.getElementById("notification");					// notification display paragraph
+	var submitButton = document.getElementById("contactbutton");			// submit button on form
+	var errMsg = "Please ensure all fields are complete :)";				// error message to display for any alert dialogs (just 1 for now)
+
+	// This function is called on a successful form submission. This will open the acknowledgemet.html page in the current browser window then
+	// halt the form from executing any further action. Without it being done this way with the "onsubmit" tag on the form element, my redirect
+	// would always come back as "page not found". I think the form is trying to post its data and cant (since I have no way to handle it) and
+	// the form action has to be stopped to avoid that outcome?
+	function redirect() {
+		window.open("msgconfirm.html", "_self");
+		console.log("Success - All three fields filled, email in correct format."); 
+		return false;
+	}
+	
+	// the "final check" function. Since fields load as white-colored with no indication they are required, a final check is
+	// performed when the user tries to submit the form which will alert the user to the required input fields... I hope.
+	function verifyInput() {
+		validateName();
+		validateEmail();
+		validateMessage();
+		if (nameBox.value && emailBox.value && messageBox.value) {
+			console.log("All three fields have a value. If page is not redirected, email is formatted incorrectly (handled by browser)."); 
+			return true;
+		} else {
+			console.log("One or more fields is empty - show alert dialog here");
+			alert(errMsg);
+			return false;
+			//alert("Ensure all fields are completed and email is in proper you@yoursite.com format.");
+		}
+	}
+	
+	// check if name field has an entry, recolor background and show error message display if not. 
+	function validateName() {
+		if (!nameBox.value) {
+			nameBox.style.backgroundColor = "#F7F4B0";
+			console.log("missing name text");
+			} else {
+				nameBox.style.backgroundColor = "";
+				console.log("successfully entered name text");
+			}
+	}
+
+	// check if email field has an entry, recolor background and show error message display if not. 
+	function validateEmail() {
+		if (!emailBox.value) {
+			emailBox.style.backgroundColor = "#F7F4B0";
+			console.log("missing email text");
+		} else {
+			emailBox.style.backgroundColor = "";
+			console.log("successfully entered email text");
+			}
+	}
+	
+	// check if message field has an entry, recolor background and show error message display if not. 
+	function validateMessage() {
+		if (!messageBox.value) {
+			messageBox.style.backgroundColor = "#F7F4B0";
+			console.log("missing message text");
+		} else {
+			messageBox.style.backgroundColor = "";
+			console.log("successfully entered message text");
+			}
+	}
+
+	// create backward compatible event listeners rather than putting them in html as onchange="validateName();" etc
+	// these event listeners respond to changes in the input fields
+	function createEventListeners() {
+	if (nameBox.addEventListener) {
+		nameBox.addEventListener("input", validateName, false);
+	} else if (nameBox.attachEvent) {
+		nameBox.attachEvent("onchange", validateName);
+	}
+	if (emailBox.addEventListener) {
+		emailBox.addEventListener("input", validateEmail, false);
+	} else if (emailBox.attachEvent) {
+		emailBox.attachEvent("onchange", validateEmail);
+	}
+	if (messageBox.addEventListener) {
+		messageBox.addEventListener("input", validateMessage, false);
+	} else if (messageBox.attachEvent) {
+		messageBox.attachEvent("onchange", validateMessage);
+	}
+    }
+
+	// call the function to create event listeners for text fields on page load
+	if (window.addEventListener) {
+		window.addEventListener("load", createEventListeners, false);
+	} else if (window.attachEvent) {
+		window.attachEvent("onload", createEventListeners);
+	}
+	
+	// add backward compatible event listener to Submit button - this will again remind the user to fill out all fields
+	// if any are left empty once they submit the form
+    if (submitButton.addEventListener) {
+		submitButton.addEventListener("click", verifyInput, false); 
+    } else if (submitButton.attachEvent)  {
+		submitButton.attachEvent("onclick", verifyInput, false);
+    }
---
+++ scripts/website.contactvalidator.js
@@ -0,0 +1,89 @@
+	var nameBox = document.getElementById("name");							// name text field
+	var emailBox = document.getElementById("email");						// email text field
+	var messageBox = document.getElementById("message");					// message text field
+	var errorBox = document.getElementById("errorDisplay");					// error display paragraph
+	var submitButton = document.getElementById("contactbutton");			// submit button on form
+	
+	var errMsg = "Please ensure all highlighted fields are filled!";		// error message to display when an input is missing
+	
+	function redirect() {
+		window.open("msgconfirm.html", "_self");
+		return false;
+	}
+		
+	function verifyInput() {
+		validateName();
+		validateEmail();
+		validateMessage();
+		if (nameBox.value && emailBox.value && messageBox.value) {
+		} else {
+			errorBox.innerHTML = errMsg;
+			alert("Please ensure all fields are completed :)");
+		}
+	}
+	
+	function validateName() {
+		if (!nameBox.value) {
+			nameBox.style.backgroundColor = "#F7F4B0";
+			errorBox.innerHTML = errMsg;
+			} else {
+				removeWarningMsg();
+				nameBox.style.backgroundColor = "";
+			}
+	}
+
+	function validateEmail() {
+		if (!emailBox.value) {
+			emailBox.style.backgroundColor = "#F7F4B0";
+			errorBox.innerHTML = errMsg;
+		} else {
+			removeWarningMsg();
+			emailBox.style.backgroundColor = "";
+			}
+	}
+
+	function validateMessage() {
+		if (!messageBox.value) {
+			messageBox.style.backgroundColor = "#F7F4B0";
+			errorBox.innerHTML = errMsg;
+		} else {
+			removeWarningMsg();
+			messageBox.style.backgroundColor = "";
+			}
+	}
+	
+	function removeWarningMsg() {
+		if (nameBox.value && emailBox.value && messageBox.value) {
+			errorBox.innerHTML="<em>Fields marked with * are required.</em>";
+		}
+	}
+
+	function createEventListeners() {
+	if (nameBox.addEventListener) {
+		nameBox.addEventListener("input", validateName, false);
+	} else if (nameBox.attachEvent) {
+		nameBox.attachEvent("onchange", validateName);
+	}
+	if (emailBox.addEventListener) {
+		emailBox.addEventListener("input", validateEmail, false);
+	} else if (emailBox.attachEvent) {
+		emailBox.attachEvent("onchange", validateEmail);
+	}
+	if (messageBox.addEventListener) {
+		messageBox.addEventListener("input", validateMessage, false);
+	} else if (messageBox.attachEvent) {
+		messageBox.attachEvent("onchange", validateMessage);
+	}
+	}
+
+	if (window.addEventListener) {
+		window.addEventListener("load", createEventListeners, false);
+	} else if (window.attachEvent) {
+		window.attachEvent("onload", createEventListeners);
+	}
+	
+    if (submitButton.addEventListener) {
+		submitButton.addEventListener("click", verifyInput, false); 
+    } else if (submitButton.attachEvent)  {
+		submitButton.attachEvent("onclick", verifyInput, false);
+    }
