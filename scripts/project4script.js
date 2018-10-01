0c81a4cb86a96f4dce962e6a5e9ff5b1f727168c
Dave M <42705084+daveminkowski@users.noreply.github.com>
2018-10-01 00:11:17 -0700

    Project 4 Update
    Updated with enhanced script. If the form is submitted with a missing field, the cursor will focus on that empty field (or one of the empty fields if multiple fields need correction)

--- scripts/project4script.js
+++ scripts/project4script.js
@@ -16,6 +16,7 @@
 	// would always come back as "page not found". I think the form is trying to post its data and cant (since I have no way to handle it) and
 	// the form action has to be stopped to avoid that outcome?
 	function redirect() {
+		"use strict";
 		if (nameBox.value && emailBox.value && messageBox.value){
 		window.open("msgconfirm.html", "_self");
 		console.log("Success - All three fields filled, email in correct format."); 
@@ -26,14 +27,20 @@
 	}
 	
 	// the "final check" function. Since fields load as white-colored with no indication they are required, a final check is
-	// performed when the user tries to submit the form which will alert the user to the required input fields... I hope
+	// performed when the user tries to submit the form which will alert the user to the required input fields... I hope. I only wanted to alert when the form
+	// was submitted, not every time a field changed. The text directly above the fields will change with the input, displaying either 1. the field that needs
+	// to be filled or 2. the default notification there if all fields ARE filled.
+	
 	function verifyInput() {
-		validateName();
-		validateEmail();
+		"use strict";
 		validateMessage();
+		validateEmail();
+		validateName();
 		// these three methods are called again on form submission even if the user filled in all 3 fields correctly. while not the most elegant, complete solution, 
 		// the end result doesnt change for the user. The goal here was to make sure that the form field background color changes when it has no value, and 
-		// each of these methods already include that functionality. Could probably make a funtion just to check all three simultaneously, but... I didnt.
+		// each of these methods already include that functionality. Could probably just make a funtion that checks all three fields simultaneously, but... I didnt.
+		// I think it'd be spammy on the console logs.
+		
 		if (nameBox.value && emailBox.value && messageBox.value) {
 			console.log("All three fields have a value. If page is not redirected, email is formatted incorrectly (handled by browser).");
 		} else {
@@ -41,13 +48,14 @@
 			alert(errMsg);
 		}
 	}
-	
-// check if name field has an entry. If not, recolor background and show error message/console log. 
+// check if name field has an entry. If not, recolor background and show error message/console log and focus the cursor on the missing field. 
 	function validateName() {
+		"use strict";
 		try{
 			if (!nameBox.value) {
 				nameBox.style.backgroundColor = "#F7F4B0";
 				console.log("missing name text");
+				nameBox.focus();
 				throw "Please enter your name.";
 			} else {
 				nameBox.style.backgroundColor = "";
@@ -58,12 +66,14 @@
 			notice.innerHTML = err;
 			}
 	}
-// check if email field has an entry. If not, recolor background and show error message/console log. 
+// check if email field has an entry. If not, recolor background and show error message/console log and focus the cursor on the missing field. 
 	function validateEmail() {
+		"use strict";
 		try{
 			if (!emailBox.value) {
 				emailBox.style.backgroundColor = "#F7F4B0";
 				console.log("missing EMAIL text");
+				emailBox.focus();
 				throw "Please enter your email address.";
 			} else {
 				emailBox.style.backgroundColor = "";
@@ -74,12 +84,14 @@
 			notice.innerHTML = err;
 			}
 	}
-// check if message field has an entry. If not, recolor background and show error message/console log. 
+// check if message field has an entry. If not, recolor background and show error message/console log and focus the cursor on the missing field. 
 	function validateMessage() {
+		"use strict";
 		try{
 			if (!messageBox.value) {
 				messageBox.style.backgroundColor = "#F7F4B0";
 				console.log("missing MESSAGE text");
+				messageBox.focus();
 				throw "Please enter a message.";
 			} else {
 				messageBox.style.backgroundColor = "";
@@ -90,50 +102,20 @@
 			notice.innerHTML = err;
 			}
 	}
-
 // reset notification text
+// I should probably have made the notice.inner.html text a string variable and used it to fill on page load, and then I could copy the variable here
+// in this method. Next time.
 	function resetNotice() {
+		"use strict";
 		if (nameBox.value && emailBox.value && messageBox.value) {
 			notice.innerHTML='Fields marked with <span class="warn">*</span> are required.';
 		}
 	}
 
-//	Old & Busted, no try/catch blocks
-//	function validateName() {
-//		if (!nameBox.value) {
-//			nameBox.style.backgroundColor = "#F7F4B0";
-//			console.log("missing name text");
-//			} else {
-//				nameBox.style.backgroundColor = "";
-//				console.log("successfully entered name text");
-//			}
-//	}
-//
-//	// check if email field has an entry, recolor background and show error message display if not. 
-//	function validateEmail() {
-//		if (!emailBox.value) {
-//			emailBox.style.backgroundColor = "#F7F4B0";
-//			console.log("missing email text");
-//		} else {
-//			emailBox.style.backgroundColor = "";
-//			console.log("successfully entered email text");
-//			}
-//	}
-//	
-//	// check if message field has an entry, recolor background and show error message display if not. 
-//	function validateMessage() {
-//		if (!messageBox.value) {
-//			messageBox.style.backgroundColor = "#F7F4B0";
-//			console.log("missing message text");
-//		} else {
-//			messageBox.style.backgroundColor = "";
-//			console.log("successfully entered message text");
-//			}
-//	}
-
-	// create backward compatible event listeners rather than putting them in html as onchange="validateName();" etc
+// create backward compatible event listeners rather than putting them in html as onchange="validateName();" etc
 	// these event listeners respond to changes in the input fields
 	function createEventListeners() {
+		"use strict";
 	if (nameBox.addEventListener) {
 		nameBox.addEventListener("input", validateName, false);
 	} else if (nameBox.attachEvent) {
@@ -165,3 +147,50 @@
     } else if (submitButton.attachEvent)  {
 		submitButton.attachEvent("onclick", verifyInput, false);
     }
+
+
+
+
+
+
+
+
+//	Old & Busted, no try/catch blocks
+
+
+
+//	function validateName() {
+//		if (!nameBox.value) {
+//			nameBox.style.backgroundColor = "#F7F4B0";
+//			console.log("missing name text");
+//			} else {
+//				nameBox.style.backgroundColor = "";
+//				console.log("successfully entered name text");
+//			}
+//	}
+
+//	// check if email field has an entry, recolor background and show error message display if not. 
+//	function validateEmail() {
+//		if (!emailBox.value) {
+//			emailBox.style.backgroundColor = "#F7F4B0";
+//			console.log("missing email text");
+//		} else {
+//			emailBox.style.backgroundColor = "";
+//			console.log("successfully entered email text");
+//			}
+//	}
+
+//	// check if message field has an entry, recolor background and show error message display if not. 
+//	function validateMessage() {
+//		if (!messageBox.value) {
+//			messageBox.style.backgroundColor = "#F7F4B0";
+//			console.log("missing message text");
+//		} else {
+//			messageBox.style.backgroundColor = "";
+//			console.log("successfully entered message text");
+//			}
+//	}
+
+
+
+	
